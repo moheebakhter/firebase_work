4f89221178c4d28b7c86cad2ee23916c50fc56c3
moheebakhter <131138672+moheebakhter@users.noreply.github.com>
2025-09-11 23:42:25 +0500

    updates

--- mera_project/myapp/urls.py
+++ mera_project/myapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("",views.contacts, name="con")
+  path("",views.contacts, name="con"),
+  path('show', views.Showdata, name="show"),
 ]
--- mera_project/myapp/views.py
+++ mera_project/myapp/views.py
@@ -4,24 +4,31 @@ from mera_project.firebase_config import db
 
 
 def contacts(request):
-    if request.method== "POST":
+    if request.method == "POST":
         a = request.POST.get("name")
         b = request.POST.get("email")
         c = request.POST.get("subject")
         d = request.POST.get("message")
 
-
         db.collection("contact").add({
-        "Name" : a,
-        "Email" : b,
-        "Subject" : c,
-         "Msg" : d,
-
-
+            "Name": a,
+            "Email": b,
+            "Subject": c,
+            "Msg": d,
         })
 
         messages.success(request, "Query has been Submitted")
         return redirect("con")
 
-
     return render(request, "myapp/contact.html")
+
+
+def Showdata(request):
+    mydata = db.collection("contact").stream()
+    Contact = []
+    for a in mydata:
+        convert_dict = a.to_dict()
+        Contact.append(convert_dict)
+
+    # yahan loop complete hone ke baad return
+    return render(request, "myapp/showdata.html", {"con": Contact})
